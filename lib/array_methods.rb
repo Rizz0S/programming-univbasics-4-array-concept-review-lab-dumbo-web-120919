bfcc9075269d20740cd1c4a0d2512e0d640c83f4
Summer Rizzo <srainrizzo@gmail.com>
2019-12-02 21:07:53 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -10,9 +10,26 @@ def find_element_index(array, value_to_find)
 end
 
 def find_max_value(array)
-  # Add your solution here
+  length = array.length
+  max = 0
+
+  length.times do |index|
+    if array[index] > max
+      max = array[index]
+    end
+
+  max
 end
 
 def find_min_value(array)
-  # Add your solution here
+  length = array.length
+
+  min = array[0]
+
+  length.times do |index|
+    if array[index] < min
+      min = array[index]
+    end
+
+  min
 end
